0c7cb0044cbc243ddf7655b1f416405badcc34bf
Mizan <mizan@Mizans-Mac-mini.local>
2022-05-27 18:27:05 +0600

    resetup

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
+import { environment } from './environment/environment';
+import { config } from 'dotenv';
+config();
+const ormConfig = {
+  ...environment.database,
+  entities: [User],
+} as TypeOrmModuleOptions;
 
+console.log('ormConfig', ormConfig);
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(ormConfig), UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
